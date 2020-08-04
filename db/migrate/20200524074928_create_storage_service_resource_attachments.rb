cf2fb88f7a8b2d1be88e6ec494eaf25a73735d07
Erez Theodorou <erezt@il.ibm.com>
2020-08-04 17:30:53 +0300

    Added schema changes for Autosde provider

---
+++ db/migrate/20200524074928_create_storage_service_resource_attachments.rb
@@ -0,0 +1,12 @@
+class CreateStorageServiceResourceAttachments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :storage_service_resource_attachments do |t|
+      t.references :storage_service, :type => :bigint, :index => true
+      t.references :storage_resource, :type => :bigint, :index => true
+      t.references :ems, :type => :bigint, :index => true, :references => :ext_management_system
+      t.string :ems_ref
+
+      t.timestamps
+    end
+  end
+end
