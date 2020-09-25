ae753a625e1cc6c036561b2f6edeb8fc4994bc49
GREGORYBEYER <gregoryb@il.ibm.com>
2020-09-25 09:18:33 +0300

    SDE-1385: Fixed most bugs that resulted from storage-system deprecation.

--- db/migrate/20200922180455_add_uuid_and_family_to_physical_storages.rb
+++ db/migrate/20200922180455_add_uuid_and_family_to_physical_storages.rb
@@ -1,4 +1,4 @@
-class AddUuidToPhysicalStorages < ActiveRecord::Migration[5.2]
+class AddUuidAndFamilyToPhysicalStorages < ActiveRecord::Migration[5.2]
   def change
     add_column :physical_storages, :uuid, :string
     add_reference :physical_storages, :physical_storage_family, :type => :bigint, :index => true
