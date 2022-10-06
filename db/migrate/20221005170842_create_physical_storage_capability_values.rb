ddc43955e5b303f3821158f77945e488e57825df
Or Gur <Or.Gur@ocean.ibm.com>
2022-10-06 17:44:19 +0300

    add physical storage-capability_values connection

---
+++ db/migrate/20221005170842_create_physical_storage_capability_values.rb
@@ -0,0 +1,11 @@
+class CreatePhysicalStorageCapabilityValues < ActiveRecord::Migration[6.1]
+  def change
+    create_table :physical_storage_capability_values do |t|
+      t.references :ems, :type => :bigint, :index => true, :references => :ext_management_system
+      t.string :ems_ref
+      t.references :physical_storage, :type => :bigint, :index => {:name => :index_physical_storage_cap_val_on_physical_storage_id}
+      t.references :storage_capability_value, :type => :bigint, :index => {:name => :index_physical_storage_cab_val_on_storage_capability_value_id}
+      t.timestamps
+    end
+  end
+end
