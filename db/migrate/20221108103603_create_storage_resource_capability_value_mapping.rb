42dd67db0072a9868679d6f3bf86172990da11ab
Or Gur <Or.Gur@ocean.ibm.com>
2022-11-08 13:06:44 +0200

    added many2many for capability_values and storage_resources

---
+++ db/migrate/20221108103603_create_storage_resource_capability_value_mapping.rb
@@ -0,0 +1,11 @@
+class CreateStorageResourceCapabilityValueMapping < ActiveRecord::Migration[6.1]
+  def change
+    create_table :storage_resource_capability_value_mappings do |t|
+      t.references :ems, :type => :bigint, :index => true, :references => :ext_management_system
+      t.string :ems_ref
+      t.references :storage_resource, :type => :bigint, :index => {:name => :index_storage_resource_cap_val_on_physical_storage_id}
+      t.references :storage_capability_value, :type => :bigint, :index => {:name => :index_storage_resource_cab_val_on_storage_capability_value_id}
+      t.timestamps
+    end
+  end
+end
