22eac2b1a4dccc337a3487c3c821f23428519b59
Or Gur <Or.Gur@ocean.ibm.com>
2022-11-08 13:30:32 +0200

    added many2many for capability_values and storage_services

---
+++ db/migrate/20221108110751_create_storage_service_capability_value_mapping.rb
@@ -0,0 +1,11 @@
+class CreateStorageServiceCapabilityValueMapping < ActiveRecord::Migration[6.1]
+  def change
+    create_table :storage_service_capability_value_mappings do |t|
+      t.references :ems, :type => :bigint, :index => true, :references => :ext_management_system
+      t.string :ems_ref
+      t.references :storage_service, :type => :bigint, :index => {:name => :index_service_capability_value_on_storage_service_id}
+      t.references :storage_capability_value, :type => :bigint, :index => {:name => :index_service_capability_value_on_storage_capability_value_id}
+      t.timestamps
+    end
+  end
+end
